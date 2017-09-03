8d1ee7b8e7f2777151048ece76cf497988b06e47
nvzard <nitanshu.vzard@gmail.com>
2017-09-03 14:22:59 +0530

    Added q3-assignment 1

---
+++ Assignment_1/q3.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void print(int arr[10][10]);
+
+int main(void){
+
+    srand(time(NULL));
+
+    int sum;
+    int answer_key[] = {3,1,4,1,2,3,4,2,4,1};
+    int answer_sheet[10][10];
+    
+    //Filling the answer sheet
+    for(int i=0; i<10; i++)
+        for(int j=0; j<10; j++)
+            answer_sheet[i][j] = 1 + rand()%4;
+
+    //Checking the answer sheet
+    printf("\t***Marks***\n");
+    for(int i=0; i<10; i++)
+    {
+        sum = 0;
+        for(int j=0; j<10; j++)
+            if(answer_key[j] == answer_sheet[i][j])
+                sum += 4;
+        printf("Student %d: %d \n",i+1,sum);
+
+
+    }
+//  print(answer_sheet);
+}
+
+
+
+void print(int array[10][10]){
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            printf("%d   ",array[i][j]);
+        }
+        printf("\n");
+    }
+}
